7332c0312ce72b1ca53261c02f01ffb44e4534fc
BruceMa <461657053@qq.com>
2018-02-26 21:11:15 +0800

    test

--- cn-springboot/src/main/java/cn/boot/onlyo/App.java
+++ cn-springboot/src/main/java/cn/boot/onlyo/App.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.ImportResource;
 @EnableAutoConfiguration
 @ImportResource({ "classpath:spring-shiro.xml" })
 public class App {
-
     public static void main(String[] args) {
         SpringApplication.run(App.class);
     }
